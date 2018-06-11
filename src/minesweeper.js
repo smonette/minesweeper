53a77fdeec7eae8d98bb20288705dacd66ee4d6e
Steph monette <steph@patreon.com>
2018-06-11 11:26:51 -0700

    second lesson, set up gameboard

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,20 @@
-const blankLine = ' | | '
+const printBoard = (board) => {
+  console.log('Current Board:')
+  console.log(board[0].join('|'))
+  console.log(board[1].join('|'))
+  console.log(board[2].join('|'))
 
-console.log('This is what an empty board would look like')
+}
 
-console.log(blankLine)
-console.log(blankLine)
-console.log(blankLine)
+let board = [
+              [' ',' ',' '],
+              [' ',' ',' '],
+              [' ',' ',' ']
+            ];
 
-const guessLine = '1| | '
-const bombLine =  ' |B| '
+printBoard(board)
 
-console.log('This is what a board with a guess and a bomb on it would look like:')
-console.log(guessLine)
-console.log(bombLine)
-console.log(blankLine)
+board[0][1] = '1'
+board[2][2] = 'B'
+
+printBoard(board)
