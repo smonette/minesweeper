2220f3596ad62321c37d8d6e1b35d0d450306cd0
Steph monette <steph@patreon.com>
2018-06-22 17:00:28 -0700

    adding more updates

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -30,18 +30,47 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   while (numberOfBombsPlaced < numberOfBombs){
     let randomRowIndex = Math.floor(Math.random() * numberOfRows)
     let randomColumnIndex = Math.floor(Math.random() * numberOfColumns)
-
-    board[randomRowIndex][randomColumnIndex] = "B";
-
-    numberOfBombsPlaced++;
+    if (board[randomRowIndex][randomColumnIndex] != "B") {
+      board[randomRowIndex][randomColumnIndex] = "B";
+      numberOfBombsPlaced++;
+    }
   }
   return board;
+}
+
+const getNumberOfNeighboBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [ 
+    [-1,-1],
+    [-1,0],
+    [-1,1],
+    [0,-1],
+    [0,0],
+    [0,1],
+    [1,-1],
+    [1,1]
+  ]
+  const numberOfRows = bombBoard.length
+  const numberOfColumns = bombBoard[0].length
+  let numberOfBombs = 0
+
+  neighborOffsets.forEach(offset =>{
+    const neighborRowIndex = rowIndex + offset
+    const neighborColumnIndex = columnIndex + offset
+
+    if (neighborRowIndex>= 0 && neighborRowIndex !>){
+
+    }
+  })
 }
 
 const printBoard = board => {
   console.log(board.map(row => row.join(' | ')).join('\n'))
 
 }
+
+
+
+
 
 let playerBoard = generatePlayerBoard(3,4)
 let bombBoard = generateBombBoard(3,4,5)
