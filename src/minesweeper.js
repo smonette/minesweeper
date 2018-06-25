2b7a107573610e47d2d76b73e9918501a98e3a29
Steph Monette <steph@muledesign.com>
2018-06-24 22:52:23 -0700

    starting pt 4

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -69,14 +69,14 @@ const getNumberOfNeighboBombs = (bombBoard, rowIndex, columnIndex) => {
   return numberOfBombs
 } 
 
-const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
-  if(playerBoard[rowIndex][columnIndex] != '') {
+flipTile = (rowIndex, columnIndex) => {
+  if(playerBoard[this._rowIndex][this._columnIndex] != '') {
     console.log('This tile has already been flipped')
     return
-  } else if(playerBoard[rowIndex][columnIndex] == 'B') {
-     playerBoard[rowIndex][columnIndex] == 'B'
+  } else if(playerBoard[this._rowIndex][this._columnIndex] == 'B') {
+     playerBoard[this._rowIndex][this._columnIndex] == 'B'
   } else {
-    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard,rowIndex,columnIndex)
+    playerBoard[this._rowIndex][this._columnIndex] = this.getNumberOfNeighborBombs(this._rowIndex,this._columnIndex)
   }
 }
 
@@ -84,9 +84,6 @@ const printBoard = board => {
   console.log(board.map(row => row.join(' | ')).join('\n'))
 
 }
-
-
-
 
 let playerBoard = generatePlayerBoard(3,4)
 let bombBoard = generateBombBoard(3,4,5)
@@ -100,3 +97,30 @@ printBoard(bombBoard)
 flipTile(playerBoard, bombBoard,0,0)
 console.log("Updated Player board: ")
 printBoard(playerBoard)
+
+
+
+
+
+
+
+class Board {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs){
+    this._numberOfRows = numberOfRows;
+    this._numberOfColumns = numberOfColumns;
+    this._numberOfBombs = numberOfBombs;
+    this._numberOfTiles = numberOfRows * numberOfColumns;
+    this._playerBoard = generatePlayerBoard(numberOfRows, numberOfColumns);
+    this._bombBoard = generateBombBoard(numberOfRows, numberOfColumns, numberOfBombs);
+  }
+
+  get playerBoard(){
+    return this._playerBoard
+  }
+
+  flipTile() {
+
+  }
+}
+
+
